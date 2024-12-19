98589ca0d2b4ee1f2c7fe8737fa842a92576b68f
gheorghe835 <gheorghe835@gmail.com>
2024-12-19 08:36:09 +0200

    merge mater

--- src/produs/Produs.java
+++ src/produs/Produs.java
@@ -40,15 +40,13 @@ public Produs(String name, double price, int stock) {
      public void addDiscount(double precent){
         if (precent>0 & precent<=100) {
             price -= price*(precent/100);
-            System.out.println("Discount de " + precent + " % aplicat. 
-            Pret actual: " + price);
+            System.out.println("Discount de " + precent + " % aplicat. Pret actual: " + price);
         }
         else System.out.println("Procentul rebuie sa fie intre 0 si 100 ");
      }
      //Metoda pentru a afisa un produs
      public void showDetails(){
-        System.out.println("Produsul: " + name + ", Pretul: " + price + ", 
-        Stocul: " + stock);
+        System.out.println("Produsul: " + name + ", Pretul: " + price + ", Stocul: " + stock);
      }
 
 }
